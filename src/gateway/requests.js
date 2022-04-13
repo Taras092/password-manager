1be575e271c445e224d735e1300953e1262e64eb
taras <taraslychynets@icloud.com>
2022-04-14 01:13:24 +0300

    requests file

--- src/gateway/requests.js
+++ src/gateway/requests.js
@@ -1,37 +1,85 @@
-const baseUrl = "https://61b8921e64e4a10017d19082.mockapi.io/api/v1/events";
+const baseUrl = 'https://61b8921e64e4a10017d19082.mockapi.io/api/v1/users';
 
-export const createEvent = (eventData) => {
+export const createUser = userData => {
   return fetch(baseUrl, {
-    method: "POST",
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
-    body: JSON.stringify(eventData),
-  }).then((response) => {
+    body: JSON.stringify(userData),
+  }).then(response => {
     if (!response.ok) {
-      throw new Error("Faild to create event");
+      throw new Error('Faild to create User');
     }
+    return response.json();
   });
 };
 
-export const fetchEventsCalendare = () => {
+export const fetchUsersData = () => {
   return fetch(baseUrl)
-    .then((res) => {
+    .then(res => {
       if (res.ok) {
         return res.json();
       }
     })
-    .then((eventsCalendare) =>
-      eventsCalendare.map(({ _id, ...event }) => ({ id: _id, ...event }))
-    );
+    .then(usersData => usersData.map(({ _id, ...event }) => ({ id: _id, ...event })));
 };
 
-export const deleteEvent = (eventId) => {
-  return fetch(`${baseUrl}/${eventId}`, {
-    method: "DELETE",
-  }).then((response) => {
+export const findUser = userId => {
+  return fetch(`${baseUrl}/${userId}`).then(response => response.json())
+}
+
+export const updateUser = (userId, userData) => {
+  return fetch(`${baseUrl}/${userId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(userData),
+  }).then(response => {
     if (!response.ok) {
-      throw new Error("Faild to create event");
+      throw new Error('Faild to put user');
     }
+    return response.json();
+  });
+};
+
+export const createCredential = (userId, credentialData) => {
+  return fetch(`${baseUrl}/${userId}/credentials`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(credentialData),
+  }).then(response => {
+    if (!response.ok) {
+      throw new Error('Faild to create User');
+    }
+    return response.json();
+  });
+};
+
+export const fetchCredentialsData = (userId) => {
+  return fetch(`${baseUrl}/${userId}/credentials`)
+    .then(res => {
+      if (res.ok) {
+        return res.json();
+      }
+    })
+    .then(usersData => usersData.map(({ _id, ...event }) => ({ id: _id, ...event })));
+};
+
+export const updateCredential = (userId, credentialId, credentialData) => {
+  return fetch(`${baseUrl}/${userId}/credentials/${credentialId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(credentialData),
+  }).then(response => {
+    if (!response.ok) {
+      throw new Error('Faild to put user');
+    }
+    return response.json();
   });
 };
